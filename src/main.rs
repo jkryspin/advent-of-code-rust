7f06272ad7e5c8f025f5655568d07882a974d366
John Kryspin <johnkryspin@gmail.com>
2024-12-21 13:29:35 -0500

    fresh start!

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,15 @@ fn main() {
 
     // Filter solutions
     let solutions: Vec<_> = empty()
-        .chain(year2015())
-        .chain(year2016())
-        .chain(year2017())
-        .chain(year2018())
-        .chain(year2019())
-        .chain(year2020())
-        .chain(year2021())
-        .chain(year2022())
-        .chain(year2023())
+        // .chain(year2015())
+        // .chain(year2016())
+        // .chain(year2017())
+        // .chain(year2018())
+        // .chain(year2019())
+        // .chain(year2020())
+        // .chain(year2021())
+        // .chain(year2022())
+        // .chain(year2023())
         .chain(year2024())
         .filter(|solution| year.is_none_or(|y: u32| y == solution.year))
         .filter(|solution| day.is_none_or(|d: u32| d == solution.day))
@@ -91,52 +91,53 @@ macro_rules! run {
     }
 }
 
-run!(year2015
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2016
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2017
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2018
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2019
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2020
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2021
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2022
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
-run!(year2023
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
-);
-
+// run!(year2015
+//     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+//     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+// );
+//
+// run!(year2016
+//     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+//     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+// );
+//
+// run!(year2017
+//     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+//     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+// );
+//
+// run!(year2018
+//     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+//     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+// );
+//
+// run!(year2019
+//     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+//     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+// );
+//
+// run!(year2020
+//     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+//     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+// );
+//
+// run!(year2021
+//     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+//     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+// );
+//
+// run!(year2022
+//     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+//     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+// );
+//
+// run!(year2023
+//     day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+//     day14, day15, day16, day17, day18, day19, day20, day21, day22, day23, day24, day25
+// );
+//
 run!(year2024
-    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
-    day14, day15, day16, day17, day18, day19, day20
+    day01
+    // day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13,
+    // day14, day15, day16, day17, day18, day19, day20
 );

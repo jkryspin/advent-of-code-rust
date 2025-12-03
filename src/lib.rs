87c8a00ea97b0ee9c474521a7916a4214ac774f2
John Kryspin <johnkryspin@gmail.com>
2025-12-02 20:26:26 -0500

    day 02

--- src/lib.rs
+++ src/lib.rs
@@ -80,5 +80,5 @@ library!(year2024 "Locate the Chief Historian in time for the big Christmas slei
 );
 
 library!(year2025 "Advent of Code 2025"
-    day01, day02
+    day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12
 );
---
+++ src/year2025/day08.rs
@@ -0,0 +1,26 @@
+//! # Day 8: [Title]
+
+pub fn part1(input: &str) -> usize {
+    0
+}
+
+pub fn part2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(EXAMPLE), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(EXAMPLE), 0);
+    }
+}
